9c5dcb2406e8cead1dd742c3ab4b2dff62e0a7f5
Jacques Peron <cataclop@hotmail.com>
2015-05-09 12:15:07 +0200

    Nettoyage encore

--- lyluatex.lua
+++ lyluatex.lua
@@ -14,21 +14,50 @@ TMP = 'tmp_ly'
 N = 0
 
 
-local function lilypond(entree, sortie)
-    os.execute(string.format(
+function direct_ly(ly, largeur, facteur)
+    N = N + 1
+    sortie = TMP..'/f'..N
+    compiler_ly(entete_lilypond(facteur, largeur - 10)..'\n'..ly:gsub('\\par ',''), sortie)
+    retour_tex(sortie)
+end
+
+
+function inclure_ly(entree, largeur, facteur)
+    nom = splitext(entree, 'ly')
+    entree = nom..'.ly'
+    if not lfs.isfile(entree) then err("Le fichier %s n'existe pas.", entree) end
+    sortie = TMP..'/' ..nom..'-'..facteur..'-'..string.gsub(largeur, '%.', '-')..'.ly'
+    sortie = splitext(sortie, 'ly')
+    if not lfs.isfile(sortie..'-systems.tex')
+    or lfs.attributes(sortie..'-systems.tex').modification < lfs.attributes(entree).modification
+    then
+	i = io.open(entree, 'r')
+	ly = i:read('*a')
+	i:close()
+	compiler_ly(entete_lilypond(facteur, largeur - 10)..'\n'..ly, sortie)
+    end
+    retour_tex(sortie)
+end
+
+
+function compiler_ly(ly, sortie)
+    mkdirs(dirname(sortie))
+    local p = io.popen(
 	"lilypond "
 	.."-dno-point-and-click "
 	.."-dbackend=eps "
 	.."-djob-count=2 "
 	.."-ddelete-intermediate-files "
-	.."-o %s %s",
-	sortie,
-	entree
-    ))
+	.."-o "..sortie
+	.." -",
+	'w'
+    )
+    p:write(ly)
+    p:close()
 end
 
 
-local function entete_lilypond(facteur, largeur)
+function entete_lilypond(facteur, largeur)
     return string.format(
 [[%%En-tête
 \version "2.18.2"
@@ -72,35 +101,8 @@ largeur
 end
 
 
-function direct_ly(ly, largeur, facteur)
-    N = N + 1
-    tmp = TMP..'/f'..N..'.ly'
-    f = io.open(tmp, 'w')
-    f:write(ly)
-    f:close()
-    inclure_ly(tmp, largeur, facteur)
-end
-
-
-function inclure_ly(entree, largeur, facteur)
-    nom = splitext(entree, 'ly')
-    entree = nom..'.ly'
-    if not lfs.isfile(entree) then err("Le fichier %s n'existe pas.", entree) end
-    sortie = TMP..'/' ..nom..'-'..facteur..'-'..string.gsub(largeur, '%.', '-')..'.ly'
-    snom = splitext(sortie, 'ly')
-    mkdirs(dirname(sortie))
-    if not lfs.isfile(snom..'-systems.tex')
-    or lfs.attributes(sortie).modification < lfs.attributes(entree).modification
-    then
-	i = io.open(entree, 'r')
-	o = io.open(sortie, 'w')
-	o:write(entete_lilypond(facteur, largeur - 10))
-	o:write(i:read('*a'))
-	o:close()
-	i:close()
-	lilypond(sortie, snom)
-    end
-    i = io.open(snom..'-systems.tex', 'r')
+function retour_tex(sortie)
+    i = io.open(sortie..'-systems.tex', 'r')
     contenu = i:read("*all")
     i:close()
     texoutput, _ = string.gsub(
