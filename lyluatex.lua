e3cd29513f6f9532a2c6fdacce2de56eaa65338a
Urs Liska <git@ursliska.de>
2018-02-07 10:51:45 +0100

    Respect "% begin verbatim" comments
    If LilyPond code contains
    % begin verbatim and optionally
    % end verbatim
    
    Only the part inside that will be printed verbatim.
    
    NOTE: I'm not 100% sure if that fully matches lilypond-book behaviour
    because we *only* search for the exact construct "% begin verbatim"
    and not ' "begin verbatim" within a comment" as the lilypond-book docs
    state (but I doubt lilypond-book does that additional analysis).

--- lyluatex.lua
+++ lyluatex.lua
@@ -374,7 +374,8 @@ function Score:calc_verbatim()
   \end{verbatim}
 %s
         ]],
-        self.ly_code,
+        self.ly_code:gsub('.*%% begin verbatim', ''):gsub(
+            '%% end verbatim.*', ''),
         intertext)
     else self.verbatim = ''
     end
